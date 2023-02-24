428367d4327c3bc1698af6174a04ac8dee1c27e8
veydoge <veydoge@gmail.com>
2023-02-25 00:01:16 +0300

    brand new code

--- PythonGitLAB/PythonGitLAB.py
+++ PythonGitLAB/PythonGitLAB.py
@@ -1,5 +1,17 @@
-print("Hello Python from Visual Studio!")
-s = "*"*30
-print(s)
-print("New project")
-print(s)
+try:
+    class square:
+        def __init__(self, side):
+            self.side = side
+        def place(self):
+            return self.side * self.side
+        def perimeter(self):
+            return 4 * self.side
+
+    myobj = square(int(input("¬ведите сторону первого квадрата\n")))
+    myobj2 = square(int(input("¬ведите сторону второго квадрата\n")))
+    print(f"ѕлощадь первого квадрата = {myobj.place()}")
+    print(f"ѕериметр второго квадрата = {myobj2.perimeter()}") #вообще метод, обьект и класс
+    # стоило бы называть square, тк обьект - квадрат, класс - квадрат, площадь - переводитс€ также как "квадрат",
+    # но это не нагл€дно
+except ValueError:
+    print("¬ведено не число")
